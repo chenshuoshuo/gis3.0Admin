f23d7ed887ba8741a6e38669f3c4798e3d92363d
shenghao <personal_sh@163.com>
2019-09-19 10:04:35 +0800

    feature/sh/v1.0.4

--- static/config.js
+++ static/config.js
@@ -1,9 +1,9 @@
 // CMCCR CMIPS CMDBE配置项
 window.g = {
   // 中控发布平台接口地址
-  BASE_CCR: 'http://192.168.4.241/cmccr-server',
+  BASE_CCR: 'http://testgis.you07.com/cmccr-server',
   // 地图门户管理接口地址
-  BASE_IPS: 'http://192.168.4.241:6062',
+  BASE_IPS: 'https://testgis.you07.com/cmips-server',
   // CMGIS 接口地址
   BASE_GIS: 'https://testgis.you07.com/cmgis-server/'
 }
